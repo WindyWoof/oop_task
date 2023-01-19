87f12e6d7f8a9f726461f76b11d4809625c730bf
WindyWoof <windywoof@Air-WindyWoof.mshome.net>
2023-01-19 16:08:15 +1000

    oop4 update

--- oop_4/oop4/src/oop4.cpp
+++ oop_4/oop4/src/oop4.cpp
@@ -4,3 +4,30 @@
 #include <algorithm>
 using namespace std;
 
+struct RingList
+{
+	Element* next, *prev;  
+}
+
+struct Ring 
+{
+	Element* current = NULL;
+};
+
+void pushNext(Ring& r, int d)
+{
+	Element* e = (Element*)malloc(sizeof(Element));
+	e -> data = d;
+	e -> next = r.currnet ->next;
+	e -> prev = r.currnet;
+	r.currnet -> next -> prev = e;
+	r.current -> next = e;
+}
+
+
+
+void pushPrev(Ring& r, int d); 
+int pull(Ring& r);
+void goNext(Ring& r);
+void goPrev(Ring& r);
+void print(Ring& r);
